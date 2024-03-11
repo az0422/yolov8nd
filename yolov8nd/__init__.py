4dded7f6bf89d98adb6fb9503fa02cafc2b8878a
az0422 <87636192+az0422@users.noreply.github.com>
2024-03-11 13:51:28 +0900

    fix a DDP bug

--- yolov8nd/__init__.py
+++ yolov8nd/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.0.1"
+__version__ = "8.0.2"
 
 from yolov8nd.data.explorer.explorer import Explorer
 from yolov8nd.models import RTDETR, SAM, YOLO, YOLOWorld
