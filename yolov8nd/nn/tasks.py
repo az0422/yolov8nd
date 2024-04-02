f851b621255923cde32becfda3667c2aa2c109f7
az0422 <az0422@naver.com>
2024-04-02 22:26:00 +0900

    available pre-trained weights of YOLOv8nd

--- yolov8nd/nn/tasks.py
+++ yolov8nd/nn/tasks.py
@@ -724,6 +724,8 @@ def torch_safe_load(weight):
     check_suffix(file=weight, suffix=".pt")
     if str(weight).find("amp-test") + 1:
         file = attempt_download_asset(weight, "az0422/yolo-assets", "amp-test")
+    if str(weight).find("yolov8nd") + 1:
+        file = attempt_download_asset(weight, "az0422/yolo-assets", "v1.0-nd")
     else:
         file = attempt_download_asset(weight)  # search online if missing locally
     try:
